807980e94886850b7f7230b79633a1779f0198a7
Eric Dennison <ericd@netdenizen.com>
2023-12-01 12:32:28 -0500

    Updated from Brython Server: 12/1/2023 12:32:28 PM

--- dec01.py
+++ dec01.py
@@ -4,6 +4,8 @@ def part1(data):
     d = [filter(lambda c: c in string.digits, l) for l in data]
     return d
     
+print (__name__)
 if __name__ == '__main__':
-    with open(fnam) as file:
-        print(part1(file.readlines))
+    for fnam in ['dec01s.txt','dec01.txt']:
+        with open(fnam) as file:
+            print(part1(file.readlines))
