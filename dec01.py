0ce49c74e7255b4a8781a9ba3c664d57a9223177
Eric Dennison <ericd@netdenizen.com>
2023-12-01 12:30:41 -0500

    Updated from Brython Server: 12/1/2023 12:30:42 PM

--- dec01.py
+++ dec01.py
@@ -1,1 +1,9 @@
+import string
 
+def part1(data):
+    d = [filter(lambda c: c in string.digits, l) for l in data]
+    return d
+    
+if __name__ == '__main__':
+    with open(fnam) as file:
+        print(part1(file.readlines))
