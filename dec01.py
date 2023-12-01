f7a4fa30dce303a3f85704749338407a472a9cfd
Eric Dennison <ericd@netdenizen.com>
2023-12-01 12:32:59 -0500

    Updated from Brython Server: 12/1/2023 12:33:00 PM

--- dec01.py
+++ dec01.py
@@ -1,11 +1,11 @@
 import string
 
 def part1(data):
-    d = [filter(lambda c: c in string.digits, l) for l in data]
+    d = [filter(lambda c: c in string.digits, list(l)) for l in data]
     return d
     
 print (__name__)
-if __name__ == '__main__':
+if __name__ == 'exec':
     for fnam in ['dec01s.txt','dec01.txt']:
         with open(fnam) as file:
             print(part1(file.readlines))
